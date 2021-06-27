cf8f8d08ae93dc1d849740e73ec5db453a5860eb
EleonoraDellAmico <eleonoradellamico4@gmail.com>
2021-06-28 01:00:11 +0200

    fixed toggle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import '../assets/stylesheets/src/application.scss';
 
-const test = () => {
-	console.log('test');
-};
-test();
-
 const navToggle = document.querySelector('.services__container__title');
 const articleToggle = document.getElementsByClassName('container__dropDown__info');
+
 function Toggle() {
 	for (let index = 0; index < articleToggle.length; index++) {
 		const element = articleToggle[index];
@@ -18,11 +14,10 @@ navToggle.addEventListener('click', Toggle);
 
 //TOGGLE DETAILS
 const linkToggle = document.querySelector('.link__moreInformation');
-const detailsToggle = document.getElementById('section__details__dropDown');
+const detailsToggle = document.getElementById('container__moreDetails');
 
 function ToggleMoreInformation() {
-	detailsToggle.classList.toggle('display');
-	console.log('hice click');
+	detailsToggle.classList.toggle('show-details');
 }
 
 linkToggle.addEventListener('click', ToggleMoreInformation);
