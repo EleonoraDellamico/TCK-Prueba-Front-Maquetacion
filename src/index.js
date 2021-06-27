a57a542503c668aebbab274ff0dc62b47328753c
EleonoraDellAmico <eleonoradellamico4@gmail.com>
2021-06-27 15:17:54 +0200

    added some first part of toggle

--- src/index.js
+++ src/index.js
@@ -4,3 +4,12 @@ const test = () => {
 	console.log('test');
 };
 test();
+
+const navToggle = document.querySelector('.services__btn__dropDown');
+const articleToggle = document.querySelectorAll('.container__dropDown__info');
+
+function Toggle() {
+	articleToggle.classList.toggle('show');
+}
+
+navToggle.addEventListener('click', Toggle);
