125563678803ce8f70b2c451c139766ce4ae446f
EleonoraDellAmico <eleonoradellamico4@gmail.com>
2021-06-27 18:28:28 +0200

    added toggle dropdown tab2

--- src/index.js
+++ src/index.js
@@ -5,11 +5,24 @@ const test = () => {
 };
 test();
 
-const navToggle = document.querySelector('.services__btn__dropDown');
-const articleToggle = document.querySelectorAll('.container__dropDown__info');
-
+const navToggle = document.querySelector('.services__container__title');
+const articleToggle = document.getElementsByClassName('container__dropDown__info');
 function Toggle() {
-	articleToggle.classList.toggle('show');
+	for (let index = 0; index < articleToggle.length; index++) {
+		const element = articleToggle[index];
+		element.classList.toggle('show');
+	}
 }
 
 navToggle.addEventListener('click', Toggle);
+
+//TOGGLE DETAILS
+const linkToggle = document.querySelector('.link__moreInformation');
+const detailsToggle = document.getElementById('section__details__dropDown');
+
+function ToggleMoreInformation() {
+	detailsToggle.classList.toggle('display');
+	console.log('hice click');
+}
+
+linkToggle.addEventListener('click', ToggleMoreInformation);
