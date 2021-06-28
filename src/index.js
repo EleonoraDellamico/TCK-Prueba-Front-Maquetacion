6ca626685a2b85dc7e9a3df025275a8428482d11
EleonoraDellAmico <eleonoradellamico4@gmail.com>
2021-06-28 19:08:33 +0200

    added fuction second toggle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '../assets/stylesheets/src/application.scss';
-//TOGGLE TAB 2 COLLAPSE
+//TOGGLE TAB 2 COLLAPSE "MEJORA TU EXPERIENCIA"
 const navToggle = document.querySelector('.services__container__title');
 const articleToggle = document.getElementsByClassName('container__dropDown__info');
 
@@ -12,12 +12,23 @@ function Toggle() {
 
 navToggle.addEventListener('click', Toggle);
 
-//TOGGLE DETAILS
+//TOGGLE DETAILS TAB 2 "Olvidase de la rutina"
 const linkToggle = document.querySelector('.link__moreInformation');
-const detailsToggle = document.getElementById('container__moreDetails');
+const detailsToggle = document.getElementById('container__moreDetailsCard1');
 
 function ToggleMoreInformation() {
 	detailsToggle.classList.toggle('show-details');
 }
 
 linkToggle.addEventListener('click', ToggleMoreInformation);
+
+//TOGGLE DETAILS TAB2 "Experiencias y programas SPA con agua"
+
+const linkToggleSecondCard = document.getElementById('dropDonCard2');
+const detailsToggleSecondCard = document.getElementById('container__moreDetailsCard2');
+
+function ToggleMoreInformationSecondCard() {
+	detailsToggleSecondCard.classList.toggle('show-details');
+}
+
+linkToggleSecondCard.addEventListener('click', ToggleMoreInformationSecondCard);
